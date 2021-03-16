0b13e3e55d0663c0b3557d6f6a6c1aec22b258af
Irina_Selivanova <68749060+Bordo951@users.noreply.github.com>
2021-03-16 16:49:16 +0300

    Revert "Develop"

--- #src/js/_menu.js
+++
@@ -1,8 +0,0 @@
-let mobileMenuItems = document.querySelectorAll('.mobile-menu__item');
-
-mobileMenuItems.forEach(function (element) {
-    element.addEventListener("click", function () {
-        let menuToggle = document.getElementById('mobile-menu__toggle');
-        menuToggle.checked = false;
-    });
-});
